8c7177fc2447cec58d9d21fba9c2d519f3a789a3
arpit <arpitsrivastava54@gmail.com>
2024-05-28 16:26:30 +0530

    add api for monkey ecommerce api generation

--- next.config.mjs
+++ next.config.mjs
@@ -31,16 +31,15 @@ const nextConfig = {
      },
     ],
    },
-  //  {
-  //   source: "/",
-  //   headers: [
-  //    {
-  //     key: "Content-Security-Policy",
-  //     value:
-  //      "default-src 'self'; script-src 'self' https://example.com; style-src 'self' https://example.com; img-src 'self' https://example.com; font-src 'self' https://example.com; object-src 'none'; frame-ancestors 'none';",
-  //    },
-  //   ],
-  //  },
+   {
+    source: "/api/create-checkout-session-for-monkey",
+    headers: [
+      { key: "Access-Control-Allow-Credentials", value: "true" },
+      { key: "Access-Control-Allow-Origin", value: "*" },
+      { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+      { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+    ],
+   },
   ];
  },
 };
---
+++ src/app/api/create-checkout-session-for-monkey/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST(request: NextRequest) {
+ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2024-04-10",
+  typescript: true,
+ });
+ const items = (await request.json()).items as any[];
+
+ const lineItems = items.map(item => ({
+  price_data: {
+    currency: 'inr',
+    product_data: {
+      name: item.name,
+      description: item.description,
+      images: [item.image],
+    },
+    unit_amount: item.price * 100, // Stripe expects amount in cents
+  },
+  quantity: item.quantity || 1,
+}));
+ try {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: lineItems,
+    mode: 'payment',
+    success_url: 'https://monkeycloth.vercel.app/',
+    cancel_url: 'https://monkeycloth.vercel.app/',
+  });
+
+  return NextResponse.json(
+   { success: true, msg: "Session id Created", data: session.id },
+   { status: 200 }
+  );
+ } catch (error) {
+  return NextResponse.json(
+   { success: false, msg: "Session id Created failed" },
+   { status: 408 }
+  );
+ }
+}
